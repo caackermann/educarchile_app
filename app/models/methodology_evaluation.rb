b8551d92f8b87eba4fd11936a76120950d5b20ad
gdburky <gdburky@uc.cl>
2017-11-08 20:56:09 -0300

    project connection

--- app/models/methodology_evaluation.rb
+++ app/models/methodology_evaluation.rb
@@ -1,4 +1,4 @@
 class MethodologyEvaluation < ApplicationRecord
   belongs_to :methodology
-  belongs_to :project_connection
+  belongs_to :project_connection, optional: true
 end
--- app/models/project_connection.rb
+++ app/models/project_connection.rb
@@ -1,3 +1,8 @@
 class ProjectConnection < ApplicationRecord
   belongs_to :project
+  has_many :project_evaluations, dependent: :destroy
+  accepts_nested_attributes_for :project_evaluations,
+                                allow_destroy: true,
+                                reject_if: proc { |att| att['reason'].blank? },
+                                reject_if: proc { |att| att['methodology_id'].blank? }
 end
