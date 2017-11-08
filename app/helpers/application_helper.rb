6789aad5df78930eb5d2c66344fefaadbc1fd86e
Christian Ackermann <caackermann@uc.cl>
2017-11-07 21:37:05 -0300

    home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
-end
+    
+        # Returns the full title on a per-page basis.
+        def full_title(page_title = '')
+          base_title = "EducarChile"
+          if page_title.empty?
+            base_title
+          else
+            page_title + " | " + base_title
+          end
+        end
+    
+        def gravatar_url(email, size)
+          gravatar = Digest::MD5::hexdigest(email).downcase
+          url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+        end
+    
+      end
+    
