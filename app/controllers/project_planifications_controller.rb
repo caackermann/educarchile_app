4cd808099861be22fd2ad0c7050076c9b95bd531
cjsalame <cjsalame@uc.cl>
2017-11-09 20:07:47 -0300

    flow working till evaluation

--- app/controllers/project_planifications_controller.rb
+++ app/controllers/project_planifications_controller.rb
@@ -33,7 +33,7 @@ class ProjectPlanificationsController < ApplicationController
 
     respond_to do |format|
       if @project_planification.save
-        format.html { redirect_to new_project_project_evaluation_path(@project), notice: 'Project planification was successfully created.' }
+        format.html { redirect_to new_project_project_implementation_path(@project), notice: 'Project planification was successfully created.' }
         format.json { render :show, status: :created, location: @project_planification }
       else
         format.html { render :new }
