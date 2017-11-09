7d9139c0c664b00b68357ec2f5de2f3a10bd2fac
cjsalame <cjsalame@uc.cl>
2017-11-08 23:36:30 -0300

    avg rating done

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,13 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many   :project_reviews
+
+  def avg_rating
+    if project_reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = project_reviews.average(:rating).round(2)
+    end
+    @avg_rating
+  end
 end
