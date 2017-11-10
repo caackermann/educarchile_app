ac1339cd3269805b8a1ba9b44e7d81f33f2ef8da
cjsalame <cjsalame@uc.cl>
2017-11-09 22:09:36 -0300

    changes in choice and plan

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-
+  has_one    :methodologhy
   has_one :project_connection, inverse_of: :project
   has_one :project_choice
   has_one :project_planification, inverse_of: :project
